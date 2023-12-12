58d4df013040c21dd4134ae1366f31a9b5ee0750
angeldvilla <escarlata.2003@gmail.com>
2023-12-12 17:56:39 -0500

    New changes

--- src/utils/jwtToken.js
+++ src/utils/jwtToken.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const { COOKIE_EXPIRES_TIME } = process.env;
+
+
 // Crea y envía token y guarda en la cookie.
 const sendToken = (user, statusCode, res) => {
 
@@ -7,13 +11,14 @@ const sendToken = (user, statusCode, res) => {
     // Opciones para cookies
     const options = {
         expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+            Date.now() + COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
         httpOnly: true
     }
 
+    res.cookie('token', token, options)
 
-    res.status(statusCode).cookie('token', token, options).json({
+    res.status(statusCode).json({
         success: true,
         token,
         user
