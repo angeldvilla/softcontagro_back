d47b160c183e7a3b5dd726b648145e6267c397fb
angeldvilla <escarlata.2003@gmail.com>
2023-12-16 14:58:04 -0500

    New changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,7 +10,10 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     const { token } = req.cookies
 
     if (!token) {
-        return next(new ErrorHandler('Login first to access this resource.', 401))
+        return res.status(401).json({
+            success: false,
+            message: 'Inicie sesión primero para acceder a este recurso.'
+        })
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
@@ -23,8 +26,11 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 exports.authorizeRoles = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
-            return next(
-                new ErrorHandler(`Role (${req.user.role}) is not allowed to acccess this resource`, 403))
+            return res.status(403).json({
+                success: false,
+                message: 'Acceso no autorizado para este recurso'
+            })
+
         }
         next()
     }
